413086e5137388461f7295c8befcce76debf458f
Renjith Varvila Reghu <vrrenjith5@gmail.com>
2022-09-05 16:30:31 +0530

    Add files via upload

---
+++ sender.h
@@ -0,0 +1,15 @@
+#ifndef __SENDER_H_
+#define __SENDER_H_
+#include "sensor/temperature.h"
+#include "sensor/state_of_charge.h"
+#include <vector>
+
+#define READINGS_IN_STREAM  (50)
+
+using namespace std;
+
+bool sendSensorDataToConsole();
+
+#endif /*__SENDER_H_ */
+
+
---
+++ sender_test.cpp
@@ -0,0 +1,29 @@
+#define CATCH_CONFIG_MAIN
+#include <iostream>
+#include "sender.h"
+#include "test/catch.hpp"
+
+using namespace std;
+
+TEST_CASE("Test 1 : Check whether 50 random temperature values are generated or not") {
+  vector<int> generatedTemperatureValues = generateTemperatureValues(TEMPERATURE_MAX_VALUE ,TEMPERATURE_MIN_VALUE, READINGS_IN_STREAM);
+  bool isTemperatureBufferEmpty = generatedTemperatureValues.empty();
+  REQUIRE(isTemperatureBufferEmpty == false);
+  int temperatureBufferSize = generatedTemperatureValues.size();
+  REQUIRE(temperatureBufferSize == READINGS_IN_STREAM);
+}
+
+TEST_CASE("Test 2 : Check whether 50 random SOC values are generated or not") {
+  vector<int> generatedSocValues = generateSocValues(SOC_MAX_VALUE ,SOC_MIN_VALUE, READINGS_IN_STREAM);
+  bool isSocBufferEmpty = generatedSocValues.empty();
+  REQUIRE(isSocBufferEmpty == false);
+  int socBufferSize = generatedSocValues.size();
+  REQUIRE(socBufferSize == READINGS_IN_STREAM);
+}
+
+TEST_CASE("Test 3 : Check whether 50 random temperature, SOC values are sent to console successfully") {
+  bool isSendSensorDataToConsole = sendSensorDataToConsole();
+  REQUIRE(isSendSensorDataToConsole == true);
+}
+
+
