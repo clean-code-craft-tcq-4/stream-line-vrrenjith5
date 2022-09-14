fbe8817c4fd00bb68c3408d93234f6b9da04f638
Naveen-R-Mundaganur <105204623+Naveen-R-Mundaganur@users.noreply.github.com>
2022-09-14 12:12:28 +0530

    Update BatteryParametersReceiver.cs

--- Receiver/BatteryParametersReceiver.cs
+++ Receiver/BatteryParametersReceiver.cs
@@ -3,12 +3,15 @@ using System.Collections.Generic;
 
 namespace BatteryParametersReceiver
 {
-    public string DisplayData()
-    {        
-        Console.WriteLine("Hello");
-        string str=Console.ReadLine();
-        Console.WriteLine(str);
-        Console.ReadLine();
-        return str;
+    public class BatteryDataReceiver
+    {
+        public string DisplayData()
+        {        
+            Console.WriteLine("Hello");
+            string str=Console.ReadLine();
+            Console.WriteLine(str);
+            Console.ReadLine();
+            return str;
+        }
     }
 }
