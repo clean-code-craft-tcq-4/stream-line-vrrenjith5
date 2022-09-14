269af7e0f2882db361a32c09874078d615253a4b
Naveen-R-Mundaganur <105204623+Naveen-R-Mundaganur@users.noreply.github.com>
2022-09-14 12:02:53 +0530

    Update BatteryParametersReceiver.cs

--- Receiver/BatteryParametersReceiver.cs
+++ Receiver/BatteryParametersReceiver.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 
 namespace BatteryParametersReceiver
 {
+    Console.WriteLine("Hello, Welcome to Stream-Line Project");
     string str=Console.ReadLine();
     Console.WriteLine(str);
     Console.ReadLine();
