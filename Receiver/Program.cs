9bc48aff69d780882cc2a939f95fa993db2052af
Naveen-R-Mundaganur <105204623+Naveen-R-Mundaganur@users.noreply.github.com>
2022-10-09 22:55:20 +0530

    Update and rename BatteryParametersReceiver.cs to Program.cs

---
+++ Receiver/Program.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+
+namespace BatteryParametersReceiver
+{
+    internal class Program
+    {        
+        static void Main(string[] args)
+        {
+            const int MaxSenderData = 50;
+            List<string> battery_Info_From_Sender_System=null;
+            
+            battery_Info_From_Sender_System = new List<string>();
+            try
+            {
+                for (int console_info = 0; console_info < MaxSenderData; console_info++)
+                {
+                    battery_Info_From_Sender_System.Add(Convert.ToString(Console.ReadLine()));
+                }
+               List<BatterySystemParameter> battery_system_info = ProcessSenderData.ProcessSenderDataStatistcs(battery_Info_From_Sender_System); 
+               List<int> battery_system_statistics= BatterySystemStastics.ComputeBatteryManagementStastisticsResult(battery_system_info);
+               BatterySystemStastics.DisplayBatteryStatisticsOutput(battery_system_statistics);
+            }
+            catch(Exception ex)
+            {
+                throw new Exception("Data passed from sender is Invalid!");
+            }
+        }
+    }
+}
