fa1f451d3b1e168f877c6e756f5376c18cbcd0cf
Naveen-R-Mundaganur <105204623+Naveen-R-Mundaganur@users.noreply.github.com>
2022-10-09 23:01:37 +0530

    Create BatterySystemParameter.cs

---
+++ Receiver/BatterySystemParameter.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace BatteryParametersReceiver
+{
+    public class BatterySystemParameter
+    {
+        public int BatteryTemperature { get; set; }
+        public int StateOfCharge { get; set; }       
+    }
+}
+
+
+
