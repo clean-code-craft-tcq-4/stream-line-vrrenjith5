3dad9953bcba22b2a31ba3703f109c53ef1f52b2
Naveen-R-Mundaganur <105204623+Naveen-R-Mundaganur@users.noreply.github.com>
2022-10-09 23:20:35 +0530

    Update ProcessSenderData.cs

--- Receiver/ProcessSenderData.cs
+++ Receiver/ProcessSenderData.cs
@@ -13,7 +13,7 @@ namespace BatteryParametersReceiver
                 string[] battery_spilt_info = sender_info.Split(',');
                 BatterySystemParameter battery_system_info = new BatterySystemParameter();
                 battery_system_info.BatteryTemperature = Convert.ToInt32(battery_spilt_info[0].Trim());
-                battery_system_info.STateOfCharge = Convert.ToInt32(battery_spilt_info[1].Trim());
+                battery_system_info.StateOfCharge = Convert.ToInt32(battery_spilt_info[1].Trim());
                 battery_parameters_data.Add(battery_system_info);
             }
             return battery_parameters_data;
