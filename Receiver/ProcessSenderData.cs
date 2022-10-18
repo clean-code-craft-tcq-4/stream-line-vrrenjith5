66015d392a51b3543349385cd760e3d61f6c73ff
Naveen-R-Mundaganur <105204623+Naveen-R-Mundaganur@users.noreply.github.com>
2022-10-19 01:21:52 +0530

    Update ProcessSenderData.cs

--- Receiver/ProcessSenderData.cs
+++ Receiver/ProcessSenderData.cs
@@ -7,16 +7,23 @@ namespace BatteryParametersReceiver
     {
         public static List<BatterySystemParameter> ProcessSenderDataStatistcs(List<string> sender_system_data)
         {
-            List<BatterySystemParameter> battery_parameters_data = new List<BatterySystemParameter>();
-            foreach (string sender_info in sender_system_data)
+            if(sender_system_data!=null && sender_system_data.Count!=0)
             {
-                string[] battery_spilt_info = sender_info.Split(',');
-                BatterySystemParameter battery_system_info = new BatterySystemParameter();
-                battery_system_info.BatteryTemperature = Convert.ToInt32(battery_spilt_info[0].Trim());
-                battery_system_info.StateOfCharge = Convert.ToInt32(battery_spilt_info[1].Trim());
-                battery_parameters_data.Add(battery_system_info);
+                List<BatterySystemParameter> battery_parameters_data = new List<BatterySystemParameter>();
+                foreach (string sender_info in sender_system_data)
+                {
+                    string[] battery_spilt_info = sender_info.Split(',');
+                    BatterySystemParameter battery_system_info = new BatterySystemParameter();
+                    battery_system_info.BatteryTemperature = Convert.ToInt32(battery_spilt_info[0].Trim());
+                    battery_system_info.StateOfCharge = Convert.ToInt32(battery_spilt_info[1].Trim());
+                    battery_parameters_data.Add(battery_system_info);
+                }
+                return battery_parameters_data;
             }
-            return battery_parameters_data;
+            else
+            {
+                return null;
+            }
         }
     }
 }
