29be3e441910aa99ae88c942ec764ea403185ba9
Naveen-R-Mundaganur <105204623+Naveen-R-Mundaganur@users.noreply.github.com>
2022-10-19 02:21:11 +0530

    Update BatterySystemParameter.cs

--- Receiver/BatterySystemParameter.cs
+++ Receiver/BatterySystemParameter.cs
@@ -4,15 +4,30 @@ namespace BatteryParametersReceiver
 {
     public class BatterySystemParameter
     {
+        private const int SOC_MIN = 0;
+        private const int SOC_MAX = 100;
+        private const int TEMP_MIN = -50;
+        private const int TEMP_MAX = 150;        
+        
         public int BatteryTemperature { get; set; }
         public int StateOfCharge { get; set; }     
         
-        public bool ValidateBatterySystemParameterBoundaries(int parameter)
+        //Check the boundary limit of Temperature
+        public bool ValidateBatterySystemTemperatureBoundaries(int parameter)
         {
-            if(parameter<int.MaxValue && parameter > int.MinValue)
+            if(parameter<TEMP_MAX && parameter > TEMP_MIN)
                 return true;
             else
-                return false;
+                return false;            
+        }
+        
+        //Check the boundary limit of StateOfCharge
+        public bool ValidateBatterySystemStateOfChargeBoundaries(int parameter)
+        {
+            if(parameter<SOC_MAX && parameter > TEMP_MIN)
+                return true;
+            else
+                return false;            
         }
     }
 }
