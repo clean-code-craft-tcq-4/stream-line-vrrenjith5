09aedcbf40102b98c915dd6c56670ce2c84ef9b3
Naveen-R-Mundaganur <105204623+Naveen-R-Mundaganur@users.noreply.github.com>
2022-10-19 02:01:48 +0530

    Update BatterySystemParameter.cs

--- Receiver/BatterySystemParameter.cs
+++ Receiver/BatterySystemParameter.cs
@@ -5,7 +5,15 @@ namespace BatteryParametersReceiver
     public class BatterySystemParameter
     {
         public int BatteryTemperature { get; set; }
-        public int StateOfCharge { get; set; }       
+        public int StateOfCharge { get; set; }     
+        
+        public bool ValidateBatterySystemParameterBoundaries(int parameter)
+        {
+            if(parameter<int.MaxValue && parameter > int.MinValue)
+                return true;
+            else
+                return false;
+        }
     }
 }
 
