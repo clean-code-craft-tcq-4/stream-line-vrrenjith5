87411c6fb0a27b1a771c42aec67a960e6bd33875
Naveen-R-Mundaganur <105204623+Naveen-R-Mundaganur@users.noreply.github.com>
2022-10-19 02:55:33 +0530

    Update ProcessSenderData.cs

--- Receiver/ProcessSenderData.cs
+++ Receiver/ProcessSenderData.cs
@@ -8,8 +8,7 @@ namespace BatteryParametersReceiver
         public static List<BatterySystemParameter> ProcessSenderDataStatistcs(List<string> sender_system_data)
         {
             if(sender_system_data!=null && sender_system_data.Count!=0)
-            {
-                bool IsValidBoudaryData=true;
+            {                
                 List<BatterySystemParameter> battery_parameters_data = new List<BatterySystemParameter>();
                 foreach (string sender_info in sender_system_data)
                 {
@@ -17,22 +16,28 @@ namespace BatteryParametersReceiver
                     BatterySystemParameter battery_system_info = new BatterySystemParameter();
                     battery_system_info.BatteryTemperature = Convert.ToInt32(battery_spilt_info[0].Trim());                    
                     battery_system_info.StateOfCharge = Convert.ToInt32(battery_spilt_info[1].Trim());
-                    if(!ValidateBatterySystemTemperatureBoundaries(battery_system_info.BatteryTemperature) || !ValidateBatterySystemStateOfChargeBoundaries(battery_system_info.StateOfCharge))
+                    if(!CheckSystemParameterBoundaries(battery_system_info.BatteryTemperature,battery_system_info.StateOfCharge))                  
                     {
-                        IsValidBoudaryData=false;
-                        break;
+                        return null;
                     }
                     battery_parameters_data.Add(battery_system_info);
                 }
-                if(IsValidBoudaryData)
-                    return battery_parameters_data;
-                else
-                    return null;
+                return battery_parameters_data;
             }
             else
             {
                 return null;
             }
         }
+        public static bool CheckSystemParameterBoundaries(int battery_data, int soc_data)
+        {
+            bool IsBoundaryValid=true;
+            if(!BatterySystemParameter.ValidateBatterySystemTemperatureBoundaries(battery_data) || !BatterySystemParameter.ValidateBatterySystemStateOfChargeBoundaries(soc_data))
+            {
+                IsBoundaryValid=false;                 
+            }
+            return IsBoundaryValid;
+        }
+        
     }
 }
