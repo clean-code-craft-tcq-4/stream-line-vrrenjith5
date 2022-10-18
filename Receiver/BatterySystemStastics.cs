86a1baac83097827fb6ed72e54055f8b11ad0546
Naveen-R-Mundaganur <105204623+Naveen-R-Mundaganur@users.noreply.github.com>
2022-10-19 00:56:47 +0530

    Update BatterySystemStastics.cs

--- Receiver/BatterySystemStastics.cs
+++ Receiver/BatterySystemStastics.cs
@@ -19,12 +19,14 @@ namespace BatteryParametersReceiver
             public static double AverageFiveSystemTemperature { get; set; }
 
             public static double AverageFiveSystemStateOfCharge { get; set; }
+        
+            public static int BatterySystemParameterCount{get;set;}
                         
         //Method Computes the System Parameter Statistics
         public static bool ComputeBatteryManagementStastisticsResult(List<BatterySystemParameter> bmsresult_data)
         {
-            bool IsStatisticsCompleted=false;
-            if(bmsresult_data!=null && bmsresult_data.Count!=0)
+            bool IsStatisticsCompleted=false;            
+            if(bmsresult_data!=null && bmsresult_data.Count>=5)
             {
                 MinimumSystemTemperature = bmsresult_data.Min(x => x.BatteryTemperature);
                 MaximumSystemTemperature = bmsresult_data.Max(x => x.BatteryTemperature);
@@ -33,10 +35,12 @@ namespace BatteryParametersReceiver
                 AverageFiveSystemTemperature = bmsresult_data.Select(p => p.BatteryTemperature).TakeLast(5).Sum(p => p) / 5;
                 AverageFiveSystemStateOfCharge = bmsresult_data.Select(p => p.StateOfCharge).TakeLast(5).Sum(p => p) / 5;         
                 IsStatisticsCompleted=true;
+                BatterySystemParameterCount=bmsresult_data.Count;
                 return IsStatisticsCompleted;
             }
             else
             {
+                BatterySystemParameterCount=bmsresult_data.Count;
                 return IsStatisticsCompleted;
             }            
          }
@@ -44,10 +48,15 @@ namespace BatteryParametersReceiver
         //Method to display the battery system parameters
         public static bool DisplayBatteryStatisticsOutput()
         {
-            Console.WriteLine($"Minimum Temperature value: {MinimumSystemTemperature},  Maximum Temperature value: {MaximumSystemTemperature}");
-            Console.WriteLine($"Minimum State Of Charge value: {MinimumStateOfCharge},  Maximum State Of Charge value: {MaximumStateOfCharge}");
-            Console.WriteLine($"Simple moving Average of last 5 values for Temerature: {AverageFiveSystemTemperature},  Simple moving Average of last 5 values for State Of Charge: {AverageFiveSystemStateOfCharge}");
-            return true;
+            if(BatterySystemParameterCount>=5)
+            {
+                Console.WriteLine($"Minimum Temperature value: {MinimumSystemTemperature},  Maximum Temperature value: {MaximumSystemTemperature}");
+                Console.WriteLine($"Minimum State Of Charge value: {MinimumStateOfCharge},  Maximum State Of Charge value: {MaximumStateOfCharge}");
+                Console.WriteLine($"Simple moving Average of last 5 values for Temerature: {AverageFiveSystemTemperature},  Simple moving Average of last 5 values for State Of Charge: {AverageFiveSystemStateOfCharge}");
+                return true;
+            }
+            else
+                return false;
         }
     }
 }
