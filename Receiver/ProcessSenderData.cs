5f0fd52827287d43d4e451911393c4b2da99f562
Naveen-R-Mundaganur <105204623+Naveen-R-Mundaganur@users.noreply.github.com>
2022-10-19 02:33:56 +0530

    Update ProcessSenderData.cs

--- Receiver/ProcessSenderData.cs
+++ Receiver/ProcessSenderData.cs
@@ -9,16 +9,25 @@ namespace BatteryParametersReceiver
         {
             if(sender_system_data!=null && sender_system_data.Count!=0)
             {
+                bool IsValidBoudaryData=true;
                 List<BatterySystemParameter> battery_parameters_data = new List<BatterySystemParameter>();
                 foreach (string sender_info in sender_system_data)
                 {
                     string[] battery_spilt_info = sender_info.Split(',');
                     BatterySystemParameter battery_system_info = new BatterySystemParameter();
-                    battery_system_info.BatteryTemperature = Convert.ToInt32(battery_spilt_info[0].Trim());
+                    battery_system_info.BatteryTemperature = Convert.ToInt32(battery_spilt_info[0].Trim());                    
                     battery_system_info.StateOfCharge = Convert.ToInt32(battery_spilt_info[1].Trim());
+                    if(!ValidateBatterySystemTemperatureBoundaries(battery_system_info.BatteryTemperature) Or !ValidateBatterySystemStateOfChargeBoundaries(battery_system_info.StateOfCharge))
+                    {
+                        IsValidBoudaryData=false;
+                        break;
+                    }
                     battery_parameters_data.Add(battery_system_info);
                 }
-                return battery_parameters_data;
+                if(IsValidBoudaryData)
+                    return battery_parameters_data;
+                else
+                    return null;
             }
             else
             {
