5deb22cfe7d853273ca9278711e5a1512ace5761
Naveen-R-Mundaganur <105204623+Naveen-R-Mundaganur@users.noreply.github.com>
2022-10-19 00:42:16 +0530

    Update BatterySystemStastics.cs

--- Receiver/BatterySystemStastics.cs
+++ Receiver/BatterySystemStastics.cs
@@ -4,7 +4,7 @@ using System.Linq;
 
 namespace BatteryParametersReceiver
 {
-    public class BatterySystemStastics
+    public static class BatterySystemStastics
     {
             //Properties Declaration
                 
